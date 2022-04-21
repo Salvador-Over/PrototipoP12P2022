d61e8dd37f1dcd9329af99cc4a06aabca87b20d1
Salvador Over <rmartinezo@miumg.edu.com>
2022-04-21 16:11:56 -0600

    Se Agregan a rama main

--- PrototipoP12P2022/include/usuario.h
+++ PrototipoP12P2022/include/usuario.h
@@ -1,6 +1,7 @@
 #ifndef USUARIO_H
 #define USUARIO_H
 
+
 using namespace std;
 
 class usuario
@@ -19,10 +20,21 @@ class usuario
         void display();
         void modify();
         void delet();
+        void insert2();
+        void display2();
+        void modify2();
+        void delet2();
+        void insert3();
+        void display3();
+        void modify3();
+        void delet3();
+
 
     protected:
 
     private:
+
+
 };
 
 #endif // USUARIO_H
